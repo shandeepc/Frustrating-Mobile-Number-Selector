e16de352d9dfb29c4da2e1f38794ac56cc796562
Shandeep <42472392+shandeepc@users.noreply.github.com>
2021-09-16 15:17:13 +0530

    removed comments

--- script.js
+++ script.js
@@ -3,21 +3,14 @@ const range2 = document.getElementById('range2');
 
 
 range2.addEventListener('input', (e) => {
-  // Get the label (which is the nextElementSibling)
   const label = e.target.nextElementSibling;
-  // Get value of the input
   const value = +e.target.value;
-  // Get the width value of the input
   const range_width = getComputedStyle(e.target).getPropertyValue('width');
-  // Get the width value of the label
   const label_width = getComputedStyle(label).getPropertyValue('width');
-  // Remove 'px' and conver to number
   const num_width = +range_width.substring(0, range_width.length - 2);
   const num_label_width = +label_width.substring(0, label_width.length - 2);
-  // Get min and max values
   const max = +e.target.max;
   const min = +e.target.min;
-  // Calculate the left value
   const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
 
   label.style.left = `${100 + left}px`;
@@ -26,7 +19,6 @@ range2.addEventListener('input', (e) => {
   var two = '';
   var three = '';
 
-  //console.log("value : " + value);
   if (value.toString().length <= 4) {
     three = value.toString();
     while (three.length != 4) {
@@ -55,35 +47,24 @@ range2.addEventListener('input', (e) => {
       three = '0' + three;
     }
   }
-  //console.log("one : " + one);
-  //console.log("two : " + two);
-  //console.log("three : " + three);
   label.innerHTML = one + ' ' + two + ' ' + three;
 });
 
 range1.addEventListener('input', (e) => {
-  // Get the label (which is the nextElementSibling)
   const label = e.target.nextElementSibling;
-  // Get value of the input
   const value = +e.target.value;
-  // Get the width value of the input
   const range_width = getComputedStyle(e.target).getPropertyValue('width');
-  // Get the width value of the label
   const label_width = getComputedStyle(label).getPropertyValue('width');
-  // Remove 'px' and conver to number
   const num_width = +range_width.substring(0, range_width.length - 2);
   const num_label_width = +label_width.substring(0, label_width.length - 2);
-  // Get min and max values
   const max = +e.target.max;
   const min = +e.target.min;
-  // Calculate the left value
   const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
 
   label.style.left = `${left}px`;
   label.innerHTML = '+' + value;
 });
 
-// From StackOverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
